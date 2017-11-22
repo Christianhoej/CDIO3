669fb39aed13e68aab1f00f02a51c9e391749bf5
Adam <Adam@DESKTOP-IK5I8ON.hhk.dk>
2017-11-22 16:16:17 +0100

    Feltklassen (super)

--- 37_del3/src/bræt/Felter.java
+++ 37_del3/src/bræt/Felter.java
@@ -1,5 +1,15 @@
 package bræt;
 
 public class Felter {
-
+	
+	private String feltnavn;
+	private int feltnr;
+	private String billede;
+	
+	public Felter(String feltnavn, int feltnr, String billede){
+		this.feltnavn = feltnavn;
+		this.feltnr = feltnr;
+		this.billede = billede;
+	}
+	
 }
