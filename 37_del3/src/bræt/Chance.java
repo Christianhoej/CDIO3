fce37755ac83c050922b9c6c5c845cded72fb144
Christian <Christian@Christian-PC.eduroam.wireless.dtu.dk>
2017-11-27 12:38:21 +0100

    Navneskift

---
+++ 37_del3/src/bræt/Chance.java
@@ -0,0 +1,10 @@
+package bræt;
+
+public class Chance extends Felter {
+
+	//Konstruktør
+	public Chance(String feltnavn, int feltnr, String billede){
+		super(feltnavn, feltnr, billede);
+	}
+	
+}
--- 37_del3/src/bræt/Ejendom.java
+++ 37_del3/src/bræt/Ejendom.java
@@ -1,41 +1,35 @@
 package bræt;
 
-public class Ejendomme extends Felter {
+public class Ejendom extends Felter {
 
 	private boolean tilsalg;
 	private int pris;
 	private int ejer;
 	private int leje;
-	
-	
-	//Konstruktør
-	public Ejendomme(String feltnavn, int feltnr, String billede, boolean tilsalg, int pris, int ejer, int leje){
+
+	// Konstruktør
+	public Ejendom(String feltnavn, int feltnr, String billede, boolean tilsalg, int pris, int ejer, int leje) {
 		super(feltnavn, feltnr, billede);
 		this.tilsalg = tilsalg;
 		this.pris = pris;
 		this.ejer = ejer;
 		this.leje = leje;
 	}
-	
-	public void setEjer(int ejer){
+
+	public void setEjer(int ejer) {
 		this.ejer = ejer;
 	}
-	
+
 	public int getEjer() {
 		return ejer;
 	}
-	
+
 	public void setLeje(int leje) {
-		this.leje=leje;
+		this.leje = leje;
 	}
-	
+
 	public int getLeje() {
 		return leje;
 	}
-	
-	public int betalLeje () {
-		if ()
-	}
-	
-	
+
 }
