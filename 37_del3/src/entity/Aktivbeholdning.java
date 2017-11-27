19a02a234a135c43c9ea224eaa3a5d4a3aa02af1
Christian <Christian@Christian-PC.eduroam.wireless.dtu.dk>
2017-11-27 15:57:03 +0100

    Aktivbeholdning

--- 37_del3/src/entity/Aktivbeholdning.java
+++ 37_del3/src/entity/Aktivbeholdning.java
@@ -2,8 +2,29 @@ package entity;
 
 public class Aktivbeholdning {
 
+	private int anlægsAktiver;
+	private int startBeholdning;
+	private int likvideMidler;
+	
 	public Aktivbeholdning() {
 
 	}
 
+	public void setAnlægsAktiver(int anlægsAktiver) {
+		this.anlægsAktiver = anlægsAktiver;
+	}
+	
+	public int getAnlægsAktiver() {
+		return anlægsAktiver;
+	}
+	
+	public void setLikvideMidler(int likvideMidler) {
+		this.likvideMidler = likvideMidler;
+	}
+	
+	public int getLikvideMidler() {
+		return likvideMidler;
+	}
+	
+	
 }
