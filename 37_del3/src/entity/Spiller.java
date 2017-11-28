4d09abd1347e592d1499c61dacec8af82e6689dd
Christoffer <Christoffer@Chris.eduroam.wireless.dtu.dk>
2017-11-28 10:24:18 +0100

    Tilføjet navn, spillerNummer og Aktivbeholdning til spiller klasse

--- 37_del3/src/entity/Spiller.java
+++ 37_del3/src/entity/Spiller.java
@@ -1,10 +1,24 @@
 package entity;
 
-String navn;
 public class Spiller {
-
-	public Spiller() {
-		// TODO Auto-generated constructor stub
+	private String navn;
+	private int spillerNummer;
+	private Aktivbeholdning Aktivbeholdning;
+	
+	public Spiller(String navn, int spillerNummer, int Aktivbeholdning) {
+		this.navn = navn;
+		Aktivbeholdning = new Aktivbeholdning(startBeholdning,0,antalSpillerePenge);
+	}
+	
+	public String getNavn() {
+		return navn;
 	}
 
+	public int getPoint() {
+		return Aktivbeholdning.getBeholdning();
+	}
+	
+	public int getSpillerNummer() {
+		return spillerNummer;
+	}
 }
