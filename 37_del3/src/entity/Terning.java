822f0a7cc9cab25e32b3da38418a47a8456b0d61
Christian <Christian@172.17.169.165>
2017-11-30 19:45:13 +0100

    JavaDoc kommentarer til enkelte klasser

--- 37_del3/src/entity/Terning.java
+++ 37_del3/src/entity/Terning.java
@@ -1,3 +1,6 @@
+/**Terning-klassen anvendes til at angive en værdi for en terning. 
+ * Klassen er kodet således at den opretter én seks-siddet terning med værdi fra 1-6
+ */
 package entity;
 
 public class Terning {
@@ -5,7 +8,11 @@ public class Terning {
 	private int værdi;
 	private int maxVærdi = 6;
 	private int minVærdi = 1;
-	
+	/**
+	 * Konstruktøren Terning oprettes med en minimum og maksimum værdi
+	 * @param minVærdi 1
+	 * @param maxVærdi 6
+	 */
 	public Terning(int minVærdi, int maxVærdi){
 		this.maxVærdi = maxVærdi;
 		this.minVærdi = minVærdi;
@@ -17,6 +24,10 @@ public class Terning {
 	public int getVærdi() {
 		return værdi;
 	}
+	/**
+	 * Metode til kast af terning.
+	 * Returnerer en tilfældig værdi mellem 1-6
+	 */
 	public void kastTerning()
 	{
 		værdi =(int)(Math.random()*maxVærdi)+minVærdi;
