d054043b458c53cbd0c7f7dd7dfe2a9ffc93cf58
sunaJOlsen <janus-olsen91@hotmail.com>
2017-12-01 00:32:06 +0100

    It Works... it actually works!

--- 37_del3/src/nogetAndet/Vinder.java
+++ 37_del3/src/nogetAndet/Vinder.java
@@ -3,13 +3,12 @@ package nogetAndet;
 import entity.Spiller;
 
 public class Vinder {
-	private int vinderNr;
+	private String vindere;
 	private int vinderNrTotalBeholdning;
 	private int størsteLikvideBeholdning;
 
-	private String hvisUafgjort = "Spiller nr: ";
 	
-	public int testHvemVinder(Spiller[]spillerArray){
+	public String testHvemVinder(Spiller[]spillerArray){
 		int tempAktiverTotal;
 		int tempLikvid;
 		for(int i = 0; i<spillerArray.length; i++) {
@@ -19,36 +18,32 @@ public class Vinder {
 
 			
 		if(tempLikvid>størsteLikvideBeholdning) { //Hvis en likvidbeholdning er større end en anden
-				vinderNr = i;
+				vindere = (i+1) + " ";
 				størsteLikvideBeholdning = tempLikvid;
-				vinderNrTotalBeholdning = spillerArray[i].getLikvideMidler()+spillerArray[i].getAnlægsaktiver();
+				vinderNrTotalBeholdning = tempAktiverTotal;
+
 		}
 		else if(tempLikvid == størsteLikvideBeholdning) { //Hvis to likvide beholdninger er lige store
 			
 			tempAktiverTotal = spillerArray[i].getAnlægsaktiver() + spillerArray[i].getLikvideMidler();
 			if(tempAktiverTotal > vinderNrTotalBeholdning)
 			{
-				vinderNr = i;
+				vindere = (i+1) + " ";
 				vinderNrTotalBeholdning = tempAktiverTotal;
-				
+
 				
 			}
-			else if(tempAktiverTotal == vinderNrTotalBeholdning && i == 2)//i == 2 gør at else kan bruges hvis der er flere spillere tilbage og de alle tre har lige mange likvide + anlægsaktiver
-				hvisUafgjort += vinderNr + ", " + i + "har begge vundet";
-
-			else {
-				hvisUafgjort = "Alle tre resterende spillere har vundet da de har lige mange likvide midler og ejendomme";
+			else if(tempAktiverTotal == vinderNrTotalBeholdning ) {
+			vindere += " og " + (i+1);
 			}
 			
 		}
 		else {
-			//Der bør ikke ske noget med variablene, men bør der ske andet?
+			
 		}
 		}
 				
-		return vinderNr;
+		return "spiller(nr) " + vindere + " har vundet";
 	}
-	public String getHvisUafgjort() {//Metode kan bruges til at hente String med navne på uafgjort spillere.
-		return hvisUafgjort;
-	}
+	
 }
