ed89dfba55930a9e0320d80784f718be3170f815
Gunn <Gunn@Gunns-PC.hhk.dk>
2017-11-30 20:09:12 +0100

    update

--- 37_del3/src/bræt/Chancekort.java
+++ 37_del3/src/bræt/Chancekort.java
@@ -11,24 +11,36 @@ import java.util.ArrayList;
  */
 public class Chancekort {
 	private String[] kort;
+	private String[] kort1;
 	
 	public Chancekort () {
 
+		try {
+			LavChanceKort("Dansk.txt");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
 
-	public void LavChanceKort(String fil) throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader(fil));
+	public void LavChanceKort(String fil) throws IOException { //Metode opretter 24 chancekort som Strings og gemmer i arrayet "kort"
+		BufferedReader br;
+		br = new BufferedReader(new FileReader(fil));
 		ArrayList<String> linjer = new ArrayList<String>();
-		String currentLine = reader.readLine();
+		String currentLine = br.readLine();
 		while(currentLine != null) {
-		    linjer.add(currentLine);
-		    currentLine = reader.readLine();
+			linjer.add(currentLine);
+			currentLine = br.readLine();
 		}
-		reader.close();
+		br.close();
 		// Convert to a String[] 
 		kort = linjer.toArray(new String[]{});
+		
+		blandKort();
 	}
-	
+
 	public String traekKort(){
 		String kort1 = kort[0];
 		for(int i =0; i<kort.length-1;i++){
@@ -37,12 +49,12 @@ public class Chancekort {
 		kort[kort.length-1] = kort1;
 		return kort1;
 	}
-	
-	public void blandKort(){
-		String[] kort1 = kort;
+
+	private void blandKort(){
 		int i = 0;
+		kort1 = new String[kort.length];
 		while(i < kort1.length) {
-			int værdi =(int)(Math.random()*52);
+			int værdi =(int)(Math.random()*kort1.length);
 			if(kort1[værdi]== null){
 				kort1[værdi] = kort[i];
 				i++;
