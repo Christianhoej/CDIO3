58ae4e802b7a36ae4074b837460fa73e28ba2ace
Christian <Christian@Christian-PC.eduroam.wireless.dtu.dk>
2017-12-08 23:41:30 +0100

    .

--- 37_del3/src/bræt/AbstractFelter.java
+++ 37_del3/src/bræt/AbstractFelter.java
@@ -1,3 +1,6 @@
+/** Klassen er abstract da metoderne i klassen ikke er færdige
+ * De overrides i de nedarvede klasser
+ */
 package bræt;
 
 import entity.Spiller; 
@@ -5,7 +8,9 @@ import gui_main.GUI;
 
 
 public abstract class  AbstractFelter {
-
+	
+	/**Protected, så de er synlige for pakken, og for nedarvede klasser
+	 */
 	protected int feltnr;
 	protected GUI gui;
 
